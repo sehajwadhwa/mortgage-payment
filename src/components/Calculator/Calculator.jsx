6bcca019b93fe9f8f812f82220c6a89c53d776a8
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-07-15 00:18:36 -0700

    components built up

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -2,84 +2,119 @@ import React from "react";
 import CurrencyPoundIcon from "@mui/icons-material/CurrencyPound";
 import { useState } from "react";
 import IconCalculator from "../../assets/images/icon-calculator.svg";
-
+import "./Calculator.scss";
 const Calculator = () => {
   const [selectedOption, setSelectedOption] = useState("");
+  const [formData, setFormData] = useState({
+    mortgageAmount: "",
+    term: "",
+    interestRate: "",
+    mortgageType: "Repayment",
+  });
 
-  //intializing handleChange function
-  const handleChange = (e) => {
-    setSelectedOption(e.target.value);
-    console.log(e.target.value, e.target.name);
-  };
-
-  const handleSubmit = (e) =>{
-    e.preventdefault ();
+  const handleSubmit = (e) => {
+    e.preventdefault();
     console.log(selectedOption);
-    if(selectedOption === "Interest Only"){
+    if (selectedOption === "Interest Only") {
       alert("Please select an option");
-    }
-    else{
+    } else {
       console.log("Submitted");
     }
-  }
+  };
 
-  //intializing handleClear function
-  const handleClear = () => {
+  const handleClear = (e) => {
     console.log("clear");
+    setFormData({
+      mortgageAmount: "",
+      term: "",
+      interestRate: "",
+      mortgageType: "Repayment",
+    });
+    setSelectedOption("Repayment");
+  };
+  const handleChange = (e) => {
+    console.log(e.target.value, e.target.name);
+    setFormData((prev) => ({ prev, [e.target.name]: e.target.value }));
+    setSelectedOption(e.target.value);
   };
   return (
     <div className="Calculator">
       <div className="Calculator__header">
         <h1>Mortgage Calculator</h1>
-        <p onClick={handleClear}>Clear All</p>
+        <p onClick={handleClear} style={{ cursor: "pointer", color: "blue" }}>
+          Clear All
+        </p>
       </div>
       <form className="Calculator__form" onSubmit={handleSubmit}>
-        <label>Mortgage Amount</label>
-        <div className="Calculator__form__input">
-          <CurrencyPoundIcon />
-          <input type="number" placeholder="" />
-        </div>
-        <label>Mortgage Term</label>
-        <div className="Calculator__form__input">
-          <input type="number" placeholder="" />
-          <span>years</span>
-        </div>
-        <label>Interest Rate</label>
-        <div className="Calculator__form__input">
-          <input type="number" placeholder="" />
-          <span>%</span>
-        </div>
-        <label>Mortgage Type</label>
-        <div className="Calculator__form__input">
-          <div className="Calculator__form_input--radio">
-            {" "}
-            <label>
-              <input
-                type="radio"
-                name="Mortgage Type"
-                value="Repayment"
-                checked={selectedOption === "Repayment"}
-                onChange={handleChange}
-              />
-              Repayment{" "}
-            </label>
+        <article>
+          <label>Mortgage Amount</label>
+          <div className="Calculator__form__input">
+            <span>
+              <CurrencyPoundIcon />
+            </span>
+            <input
+              className="Calculator__form__input--field"
+              type="number"
+              name="mortgageAmount"
+              value={formData.mortgageAmount}
+              onChange={handleChange}
+            />
           </div>
-          <div className="Calculator__form_input--radio">
-            <label>
-              <input
-                type="radio"
-                name="Mortgage Type"
-                value="Interest Only"
-                checked={selectedOption === "Interest Only"}
-                onChange={handleChange}
-              />
-              Interest Only{" "}
-            </label>
+        </article>
+        <article>
+          <label>Mortgage Term</label>
+          <div className="Calculator__form__input">
+            <input
+              className="Calculator__form__input--field"
+              type="number"
+              value={formData.term}
+              name="term"
+              onChange={handleChange}
+            />
+            <span>years</span>
           </div>
-        </div>
+        </article>
+        <article>
+          <label>Interest Rate</label>
+          <div className="Calculator__form__input">
+            <input
+              className="Calculator__form__input--field"
+              type="number"
+              name="interestRate"
+              value={formData.interestRate}
+              onChange={handleChange}
+            />
+            <span>%</span>
+          </div>
+        </article>
+        <article>
+          <label>Mortgage Type</label>
+          <div className="Calculator__form__radio">
+            <input
+              className="Calculator__form__input--radio"
+              type="radio"
+              name="mortgageType"
+              value="Repayment"
+              checked={selectedOption === "Repayment"}
+              onChange={handleChange}
+            />
+            <label>Repayment</label>
+          </div>
+          <div className="Calculator__form__radio">
+            <input
+              className="Calculator__form__input--radio"
+              type="radio"
+              name="mortgageType"
+              value="Interest Only"
+              checked={selectedOption === "Interest Only"}
+              onChange={handleChange}
+            />
+            <label>Interest Only</label>
+          </div>
+        </article>
         <button type="submit">
           <span>
-            <img src={IconCalculator} />
+            <img src={IconCalculator} alt="Calculator Icon" />
           </span>
           Calculate Repayments
         </button>
